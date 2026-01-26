e95ae2d0b0ae0dcd5e6728a0fe4fb8ee87a2ef38
Tomas Hogan <g00467388@atu.ie>
2026-01-26 16:29:09 +0000

    Initial Commit

---
+++ exercise3.ts
@@ -0,0 +1,14 @@
+let int : number = 10; 
+let str :string = "Ireland"
+let bool : boolean = false; 
+let intArray :Array<number> = [1, 2, 3];
+let touple :Array<any> = ["dog", 12, true];
+
+console.log(int)
+console.log(str)
+console.log(bool)
+
+console.log(intArray)
+console.log(touple)
+
+
---
+++ exercise4.ts
@@ -0,0 +1,21 @@
+// return length of string
+
+function GetLength(str :string) :number
+{
+    return str.length
+}
+// return length of string excluding white spaces
+function GetLengthWithoutSpace(str :string) :number
+{
+    return str.trim().length;
+}
+//
+function GetLengthWithOrWithoutSpace(str :string, CountSpaces :boolean) :number
+{
+    return CountSpaces ? GetLength(str) : GetLengthWithoutSpace(str)
+}
+
+console.log("Counting spaces: " + GetLength(" dog "))
+console.log("Not counting spaces: " + GetLengthWithoutSpace(" dog "))
+console.log("Counting spaces: " + GetLengthWithOrWithoutSpace(" dog ", true))
+console.log("Not counting spaces: " + GetLengthWithOrWithoutSpace(" dog ", false))
