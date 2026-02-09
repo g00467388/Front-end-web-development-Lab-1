07633ee6beb7ccc967566c37bbd2ff9e2d0f6486
Tomas Hogan <g00467388@atu.ie>
2026-02-09 16:15:13 +0000

    added comments and require taskArray to be implemented by classesw

--- lab3/exercise2.ts
+++ lab3/exercise2.ts
@@ -1,28 +1,35 @@
+// Define methods and variables required for classes that implement the ITask interface
 interface ITask {
+    taskArray : Array<string>;
     addTask(task :string) : number;
     listAllTasks() : void;
     removeTask(task :string) : number;
 }
+
 class Task implements ITask 
 {
-    array : Array<string> = [];
-
+    taskArray : Array<string> = [];
+    // add task to the array and added task to standard out
     addTask(task :string) : number {
-        this.array.push(task)
+        this.taskArray.push(task)
         console.log("Added " + task)
-        return this.array.length;
+        return this.taskArray.length;
     }
     listAllTasks() : void {
-        if (this.array.length == 0)
-            console.log("array is empty!")
+        // If taskArray length is 0 do not loop over taskArray
+        if (this.taskArray.length == 0)
+            console.log("taskArray is empty!")
+        // else print out each element in the taskArray
         else
-            this.array.forEach(x => console.log(x))
+            this.taskArray.forEach(x => console.log(x))
     }
-
+    // Remove an element from the taskArray
     removeTask(task :string) : number {
-        let index = this.array.indexOf(task)
-        this.array.splice(index, 1);
-        return this.array.length;
+        // find location of task
+        let index = this.taskArray.indexOf(task)
+        // Remove task by index
+        this.taskArray.splice(index, 1);
+        return this.taskArray.length;
     }
 }
 
