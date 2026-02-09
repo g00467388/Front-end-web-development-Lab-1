f00360038902ee1842ae3de3ede0ee8ec4da32b1
Tomas Hogan <g00467388@atu.ie>
2026-02-09 16:07:58 +0000

    remove whitespace

--- lab3/exercise2.ts
+++ lab3/exercise2.ts
@@ -1,32 +1,29 @@
 interface ITask {
-
- addTask(task :string) : number;
- listAllTasks() : void;
- removeTask(task :string) : number;
-
-
+    addTask(task :string) : number;
+    listAllTasks() : void;
+    removeTask(task :string) : number;
 }
 class Task implements ITask 
 {
     array : Array<string> = [];
-    addTask(task :string) : number {
-    this.array.push(task)
-    console.log("Added " + task)
-    return this.array.length;
-}
- listAllTasks() : void {
-    if (this.array.length == 0)
-        console.log("array is empty!")
-    else
-        this.array.forEach(x => console.log(x))
-}
 
- removeTask(task :string) : number {
-    let index = this.array.indexOf(task)
-    this.array.splice(index, 1);
-    return this.array.length;
-}
-    
+    addTask(task :string) : number {
+        this.array.push(task)
+        console.log("Added " + task)
+        return this.array.length;
+    }
+    listAllTasks() : void {
+        if (this.array.length == 0)
+            console.log("array is empty!")
+        else
+            this.array.forEach(x => console.log(x))
+    }
+
+    removeTask(task :string) : number {
+        let index = this.array.indexOf(task)
+        this.array.splice(index, 1);
+        return this.array.length;
+    }
 }
 
 let x = new Task(); 
